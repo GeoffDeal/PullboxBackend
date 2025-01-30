d59b798e390576500176d27388c0325ed7039ac7
Geoff Deal <geoffdeal@proton.me>
2025-01-30 18:54:01 -0330

    Checks for and creates current tables

--- app.js
+++ app.js
@@ -7,6 +7,32 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
+// Check for tables
+const tableCheck = async () => {
+  try {
+    await pool.execute(`CREATE TABLE IF NOT EXISTS users(
+      ID INT AUTO_INCREMENT PRIMARY KEY,
+      name VARCHAR(100) NOT NULL,
+      email VARCHAR(100) NOT NULL,
+      boxnumber INT,
+      phone VARCHAR(100),
+      customer TINYINT(1) NOT NULL,
+      customertype VARCHAR(100), 
+      sublist VARCHAR(100)
+      )`);
+    await pool.execute(`CREATE TABLE IF NOT EXISTS notifications (
+      id INT AUTO_INCREMENT PRIMARY KEY, 
+      title VARCHAR(100) NOT NULL, 
+      body TEXT,
+      date DATE NOT NULL DEFAULT (CURRENT_DATE)
+      )`);
+  } catch (err) {
+    console.error("Problem checking tables: ", err);
+  }
+};
+tableCheck();
+
+// Close pool on exit
 process.on("SIGINT", async () => {
   try {
     await pool.end();
