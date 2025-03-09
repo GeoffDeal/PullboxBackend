f18a9d4574f822d4be1431f9257ad2f4e7f01546
Geoff Deal <geoffdeal@proton.me>
2025-03-09 17:11:08 -0230

    ID validation for pull end points

--- src/routes/pullRoutes.js
+++ src/routes/pullRoutes.js
@@ -7,12 +7,13 @@ import {
   getWeeksPulls,
   removePull,
 } from "../controllers/pullContoller.js";
+import { validateId } from "../utils/validate.js";
 
 const router = express.Router();
 
 router.post("/addpull", addPull);
-router.patch("/changepullamount/:id", changePullAmount);
-router.delete("/removepull/:id", removePull);
+router.patch("/changepullamount/:id", validateId, changePullAmount);
+router.delete("/removepull/:id", validateId, removePull);
 router.get("/checkpull", checkPull);
 router.get("/getuserpulls", getUserPulls);
 router.get("/getweekspulls", getWeeksPulls);
