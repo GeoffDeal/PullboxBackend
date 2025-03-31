076ee98f96e67ae1f2a0ab95a90ff516b97c89f5
Geoff Deal <geoffdeal@proton.me>
2025-03-31 12:59:07 -0230

    Fixed id mix up on products/pulls join

--- src/controllers/pullContoller.js
+++ src/controllers/pullContoller.js
@@ -88,7 +88,7 @@ export async function getUserPulls(req, res) {
   const weekEnd = calcWeekEnd(releaseSunday);
 
   try {
-    const sql = `SELECT * FROM products INNER JOIN pulls_list ON products.id = pulls_list.product_id WHERE pulls_list.user_id = ? AND products.release_date >= ? AND products.release_date < ?`;
+    const sql = `SELECT pulls_list.*, pulls_list.id AS pulls_list_id, products.* FROM products INNER JOIN pulls_list ON products.id = pulls_list.product_id WHERE pulls_list.user_id = ? AND products.release_date >= ? AND products.release_date < ?`;
     const [results] = await pool.execute(sql, [userId, releaseSunday, weekEnd]);
 
     if (results.length !== 0) {
