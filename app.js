c3c8d376c30c6b5cb87d62abc17ecd6965a82cd4
Geoff Deal <geoffdeal@proton.me>
2025-04-17 15:12:22 -0230

    Updated with dotenv for deployment

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import pool from "./src/dbConfig.js";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(cors());
