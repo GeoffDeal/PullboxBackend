7cf07f2ec3d048f00c721908b6ce6c273b315d41
Geoff Deal <geoffdeal@proton.me>
2025-05-07 21:55:02 -0230

    Updated yup id requirements to match clerk ids

--- src/schemas/pullSchema.js
+++ src/schemas/pullSchema.js
@@ -3,10 +3,8 @@ import * as yup from "yup";
 export const pullPostSchema = yup
   .object({
     userId: yup
-      .number()
-      .integer()
-      .positive()
-      .max(6, "ID is too long")
+      .string()
+      .matches(/^user_[a-zA-Z0-9]+$/, "Invalid user ID format")
       .required(),
     productId: yup.number().integer().positive().required(),
   })
--- src/schemas/subscriptionSchema.js
+++ src/schemas/subscriptionSchema.js
@@ -2,7 +2,10 @@ import * as yup from "yup";
 
 export const subscriptionPostSchema = yup
   .object({
-    userId: yup.number().max(6, "ID string too long").required(),
+    userId: yup
+      .string()
+      .matches(/^user_[a-zA-Z0-9]+$/, "Invalid user ID format")
+      .required(),
     seriesId: yup.string().required(),
   })
   .noUnknown(true, "Unknown field present");
