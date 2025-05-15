6d9f9cfc5dda5b38181212e68d54b557ea0f5d7f
Geoff Deal <geoffdeal@proton.me>
2025-05-15 16:13:35 -0230

    Changes to pool export to allow code to run normally and use jest

--- src/dbConfig.js
+++ src/dbConfig.js
@@ -2,16 +2,19 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 dotenv.config();
 
-const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQL_ROOT_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  multipleStatements: true,
-});
+const pool = mysql
+  .createPool({
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    multipleStatements: true,
+  })
+  .promise();
 
-export default pool.promise();
+export default pool;
+export { pool };
